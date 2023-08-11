73f6d73c1188ab73fa4259df911d2babb3bbacb7
Vladimir Goncharov <vova.goncharov2014@gamil.com>
2023-08-11 15:22:33 +0300

    fix: script burger menu

--- library/script.js
+++ library/script.js
@@ -4,29 +4,27 @@ const wrapperBurger = document.querySelector('.tablet__wrapper');
 
 const body = document.body;
 
-if (navigation && navigationBurger) {
-  navigationBurger.addEventListener('click', () => {
-    navigation.classList.toggle('active');
-    navigationBurger.classList.toggle('active');
-    wrapperBurger.classList.toggle('display-none');
-    body.classList.toggle('lock');
-  });
+navigationBurger.addEventListener('click', () => {
+  navigation.classList.toggle('active');
+  navigationBurger.classList.toggle('active');
+  wrapperBurger.classList.toggle('display-none');
+  body.classList.toggle('lock');
+});
 
-  wrapperBurger.addEventListener('click', e => {
-    if (e.target.classList.contains('tablet__wrapper')) {
-      navigation.classList.remove('active');
-      navigationBurger.classList.remove('active');
-      wrapperBurger.classList.add('display-none');
-      body.classList.remove('lock');
-    }
+wrapperBurger.addEventListener('click', e => {
+  if (e.target.classList.contains('tablet__wrapper')) {
+    navigation.classList.remove('active');
+    navigationBurger.classList.remove('active');
+    wrapperBurger.classList.add('display-none');
+    body.classList.remove('lock');
+  }
+})
+
+navigation.querySelectorAll('.navigation__item__link').forEach(link => {
+  link.addEventListener('click', () => {
+    navigation.classList.remove('active');
+    navigationBurger.classList.remove('active');
+    wrapperBurger.classList.add('display-none');
+    body.classList.remove('lock');
   })
-
-  navigation.querySelectorAll('.navigation__item__link').forEach(link => {
-    link.addEventListener('click', () => {
-      navigation.classList.remove('active');
-      navigationBurger.classList.remove('active');
-      wrapperBurger.classList.add('display-none');
-      body.classList.remove('lock');
-    })
-  });
-}
+});
