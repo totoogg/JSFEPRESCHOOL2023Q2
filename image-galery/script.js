4e1d01deef90ad5a3d78de29d8232bec7a4b994c
Vladimir Goncharov <vova.goncharov2014@gamil.com>
2023-09-27 09:18:17 +0300

    feat: add script for button search exanple

--- image-galery/script.js
+++ image-galery/script.js
@@ -7,8 +7,7 @@ const buttons = document.querySelectorAll('.button');
 
 buttons.forEach(x => {
   x.addEventListener('click', () => {
-    let images = document.querySelectorAll('.main__image')
-    images.forEach(x => x.remove())
+    updateMain()
     let url = `https://api.unsplash.com/search/photos?query=${x.innerHTML}&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
     getData(url)
   })
@@ -17,14 +16,18 @@ buttons.forEach(x => {
 button.addEventListener('click', () => {
   event.preventDefault();
   if (search.value) {
-    content.innerHTML = '';
+    updateMain()
     let url = `https://api.unsplash.com/search/photos?query=${search.value}&per_page=30&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo`;
     getData(url);
   } else {
-    content.innerHTML = '';
+    updateMain()
   }
 })
 
+function updateMain() {
+  let images = document.querySelectorAll('.main__image')
+    images.forEach(x => x.remove())
+}
 
 async function getData(url) {
   const res = await fetch(url);
